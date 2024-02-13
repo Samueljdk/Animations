379a296c935d6c829da888ff10a8b27a2e7c17a8
daniel.solomon <daniel.solomon@plpsd.net>
2024-02-13 11:16:39 -0600

    ball moves in a negative slope direction now

--- Animations/Bouncing Ball.cpp
+++ Animations/Bouncing Ball.cpp
@@ -1,23 +1,71 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp> // sfml is a graphics libraary for drawing the shapes
+#include <iostream>
+#include <cstdlib> // allows me to access the system cmd functions such as clear 
 using namespace sf;
-int main() {
-    RenderWindow window(VideoMode(800, 600), "SFML Circle Example");
+using namespace std;
 
-    CircleShape circle(50); // radius of 50
-    circle.setPosition(375, 275); // position on the window
+void bouncingBall() {
+ 
+     int screenWidth = 600;
+     int screenHeight = 600;
+     RenderWindow window(VideoMode(screenWidth,screenHeight), "Bouncing Ball Animation");
+     float horizontalPosition = 0;
+     float verticalPosition =0;
+     float hChange = 0;
+     float vChange = 0;
+     // drawing the circle
 
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
-        }
-        //mango
-        window.clear();
-        window.draw(circle);
-        window.display();
+     CircleShape circle(50); // radius of 50
+     
+     bool stopBouncing = false;
+     while (!stopBouncing && window.isOpen()) {
+         hChange += 0.01;
+         vChange += 0.01;
+         circle.setPosition(horizontalPosition+hChange, (horizontalPosition*(-1)+vChange)); // position on the window
+
+         
+         
+         Event event;
+         while (window.pollEvent(event)) { // keeps checking to see if the user closed the windo
+             if (event.type == Event::Closed) {
+                 window.close(); // closes the window
+             }
+         }
+         
+
+         
+       
+        window.clear(); // clears the window to prepare to make the next ball drawing
+        window.draw(circle); // draws the ball
+        window.display(); // displays the ball
     }
 
+}
+
+int main() {
+    bouncingBall();
+    /*bool stopMenu = false;
+    int choice = 0;
+    while (!stopMenu) {
+        cout << "Welcome to the animations program \n";
+        cout << "Enter 1 to see the bouncing ball animation \n";
+        cout << "Enter any other value to exit \n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            bouncingBall();
+            system("cls"); // clears the screen so that the menu can be displayed on the same spot
+
+
+            break;
+        default:
+            stopMenu = true;
+
+            break;
+        }
+
+    }*/
+    
     return 0;
 }
