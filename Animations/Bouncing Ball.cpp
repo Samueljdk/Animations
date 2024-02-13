a7feca3826bb20e46a5c8b1f112a940c9326948b
daniel.solomon <daniel.solomon@plpsd.net>
2024-02-13 16:51:37 -0600

    seems complete

--- Animations/Bouncing Ball.cpp
+++ Animations/Bouncing Ball.cpp
@@ -31,12 +31,14 @@ void bouncingBall() {
      bool reachedBottom = false;
      bool reachedRight = false;
      bool reachedTop = false;
-     int counter = 0.01;
+     bool reachedLeft = false;
+     
+  
      while (!stopBouncing && window.isOpen()) {
 
          //diagonal move to the bottom
          if (horizontalPosition < 250 && verticalPosition<500 && !reachedBottom) {
-             horizontalPosition+=0.01;
+             horizontalPosition+=0.1;
              verticalPosition = horizontalPosition *2;
 
          }
@@ -46,30 +48,42 @@ void bouncingBall() {
              
          }
          // diagonal move to the right
-         if (reachedBottom && horizontalPosition<500 && verticalPosition>250) {
-             horizontalPosition += 0.01;
-             verticalPosition -= 0.01;
+         if (reachedBottom && horizontalPosition<500 && verticalPosition>250 && !reachedRight) {
+             horizontalPosition += 0.1;
+             verticalPosition -= 0.1;
     if(horizontalPosition>=500 && verticalPosition<=250)reachedRight = true;
          }
 
          // moving diagonally to the left
-         if (reachedBottom && reachedRight && verticalPosition>0) {
-             horizontalPosition -= 0.01;
-             verticalPosition -= 0.01;
+         if (reachedBottom && reachedRight && verticalPosition>0 && !reachedTop) {
+             horizontalPosition -= 0.1;
+             verticalPosition -= 0.1;
              if (verticalPosition <= 0)reachedTop = true;
          }
         
         // moving down to left
-         if (reachedBottom && reachedRight && reachedTop && horizontalPosition>0) {
-             horizontalPosition -= 0.01;
-             verticalPosition += 0.01;
-            
+         if (reachedBottom && reachedRight && reachedTop && horizontalPosition>0 && !reachedLeft) {
+             horizontalPosition -= 0.1;
+             verticalPosition += 0.1;
+             if (horizontalPosition<=0)reachedLeft = true;
          }
-         if (horizontalPosition <= 0 && verticalPosition<=0) {
-             reachedBottom = false;
-             reachedRight = false;
-             reachedTop = false;
+
+
+         if (reachedBottom && reachedLeft && reachedRight && reachedTop && verticalPosition< 500 && horizontalPosition<500) {
+         
+             horizontalPosition+=0.1;
+            verticalPosition+=0.1;
+           
+             if (horizontalPosition > 250 || verticalPosition >= 500) {
+                 reachedBottom = false;
+                 reachedLeft = false;
+                 reachedTop = false;
+                 reachedRight = false;
+                 
+             }
          }
+        
+         
         
          
         
@@ -79,9 +93,13 @@ void bouncingBall() {
          Text text;
          text.setFont(font);
          text.setCharacterSize(24);
-         String message = "W:" + to_string(horizontalPosition) + " H: " + to_string(verticalPosition);
-         if (reachedBottom)message += "\n Reached Bottom";
-         if (reachedRight)message += "\n Reached Right";
+         String message = "X: " + to_string(horizontalPosition) + " Y: " + to_string(verticalPosition);
+       
+         if (reachedBottom)circle.setFillColor(Color::Green);
+         if (reachedRight)circle.setFillColor(Color::Yellow);
+         if (reachedTop)circle.setFillColor(Color::Red);
+         if (reachedLeft)circle.setFillColor(Color::Blue);
+         
 
          text.setString(message);
 
@@ -105,28 +123,7 @@ void bouncingBall() {
 
 int main() {
     bouncingBall();
-    /*bool stopMenu = false;
-    int choice = 0;
-    while (!stopMenu) {
-        cout << "Welcome to the animations program \n";
-        cout << "Enter 0.01 to see the bouncing ball animation \n";
-        cout << "Enter any other value to exit \n";
-        cin >> choice;
-        switch (choice)
-        {
-        case 0.01:
-            bouncingBall();
-            system("cls"); // clears the screen so that the menu can be displayed on the same spot
 
-
-            break;
-        default:
-            stopMenu = true;
-
-            break;
-        }
-
-    }*/
-    
     return 0;
 }
+
