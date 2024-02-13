1004f05386632615ae699168b65f55bc2ba8d7a3
daniel.solomon <daniel.solomon@plpsd.net>
2024-02-12 23:01:06 -0600

    Add project files.

---
+++ Animations/Bouncing Ball.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+int main() {
+    RenderWindow window(VideoMode(800, 600), "SFML Circle Example");
+
+    CircleShape circle(50); // radius of 50
+    circle.setPosition(375, 275); // position on the window
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
