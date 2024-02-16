42364e6206d56ca90b08783a6084f1ce7504d26a
daniel.solomon <daniel.solomon@plpsd.net>
2024-02-15 23:06:34 -0600

    circle pyramid works

--- Circle Pyramid/Circle Pyramid.cpp
+++ Circle Pyramid/Circle Pyramid.cpp
@@ -6,39 +6,70 @@ using namespace sf;
 using namespace std;
 
 void grid() {
-    float screenWidth = 1000;
-    float screenHeight = 600;
+    float screenWidth = 900;
+    float screenHeight = 500;
     RenderWindow window(VideoMode(screenWidth, screenHeight), "Grid Animation");
     window.setPosition(Vector2i(0, 0));
 
-    const int columns = 6;
-    const int rows = 10;
+    const int columns = 5;
+    const int rows = 9;
     float xPosition = 0;
     float yPosition = 0;
     float x,y;
-    RectangleShape squares[rows][columns];
+    const float m = -1; // the slope of line
+    float b = 400; // the vertical component in y=mx+b
     CircleShape circles[rows][columns];
+    
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            squares[i][j].setFillColor(Color::Transparent);
-            squares[i][j].setSize(Vector2f(100, 100));
-            squares[i][j].setOutlineColor(Color::White);
-            squares[i][j].setOutlineThickness(1);
-            squares[i][j].setPosition(Vector2f(xPosition, yPosition));
-            xPosition += 100;
+            x = i * 100;
+            y = j * 100;
+            circles[i][j].setPosition(x, y);
 
-            circles[i][j].setPosition(i*100, j*100);
-           
-            cout << "(" << i * 100 << "," << j * 100 << ")" << endl;
-               circles[i][j].setRadius(50);
-               circles[i][j].setOutlineThickness(1);
-            circles[i][j].setOutlineColor(Color::White);
-            circles[i][j].setFillColor(Color::Green);
-       
+            circles[i][j].setRadius(50);
+            circles[i][j].setOutlineThickness(1);
+
+            if (y == (m * x) + 400) {
+    //line formula y= -x + 400
+                circles[i][j].setFillColor(Color::Red);
+                cout << i << " turned into x: " << x << endl;
+                cout << j << "turned into y: " << y << endl;
+            }
+            else if (y == ((m * x) + 600) && y > 0) {
+                //line formula y= -x + 600 , y must be > 0 to limit the length of the second diagonal
+                circles[i][j].setFillColor(Color::Yellow);
+
+            }
+            else if (y == ((m * x) + 800) && y > 100) {
+                //line formula y= -x + 800 , y must be > 1000 to limit the length of the third diagonal
+                circles[i][j].setFillColor(Color::Blue);
+
+            }
+            else if (y == ((m * x) + 1000) && y > 200) {
+                //line formula y= -x + 1000 , y must be > 200 to limit the length of the fourth diagonal
+                circles[i][j].setFillColor(Color::Magenta);
+
+            }
+            else if (y == ((m * x) + 1200) && y > 300) {
+                //line formula y= -x + 1200 , y must be > 300 to limit the length of the fifth diagonal
+                circles[i][j].setFillColor(Color::White);
+
+            }
+            else {
+                circles[i][j].setFillColor(Color::Black);
+                circles[i][j].setOutlineColor(Color::Black);
+
+            }
         }
-        xPosition = 0; // Reset xPosition for the next row
-        yPosition += 100;
     }
+              
+
+                      
+            
+        
+    
+    
+              
    
     while (window.isOpen()) {
         Event event;
@@ -52,7 +83,7 @@ void grid() {
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                window.draw(squares[i][j]);
+       //         window.draw(squares[i][j]);
                 window.draw(circles[i][j]);
 
             }
